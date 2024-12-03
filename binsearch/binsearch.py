c50fdf8d99ddaa8bf8a6d9cdcdc66a0d7970c8bc
arushmathur <128927014+arushmathur@users.noreply.github.com>
2024-12-03 19:19:41 +0530

    29556_binsearch_updated

--- binsearch/binsearch.py
+++ binsearch/binsearch.py
@@ -133,7 +133,7 @@ def binsearch(file_path = None, packageArray = None, uldArray = None, timeout = 
         
 
         old_new_cartons = new_cartons
-        new_cartons = sorted(new_cartons, key=lambda x: (floor((x['length']*x['width']*x['height'])/100),-x['cost'],min(x['length'],x['width'],x['height'],x['weight'])))
+        new_cartons = sorted(new_cartons, key=lambda x: (floor((x['length']*x['width']*x['height'])/100),-x['cost'],min(x['length'],x['width'],x['height']),x['weight']))
         new_cartons = new_cartons[:20]
 
         if file_path is None:
